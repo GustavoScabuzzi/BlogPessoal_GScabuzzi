9223316688ccf7c5bacd60c6d3ab43678438097f
GScabuzzi <gustavo-gsg@hotmail.com>
2021-09-28 01:30:15 -0300

    [Feature Boaz - Task: ADD method atualizar e deletar]

--- src/main/java/org/generation/blogGScabuzzi/controller/PostagemController.java
+++ src/main/java/org/generation/blogGScabuzzi/controller/PostagemController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -48,6 +49,11 @@ public class PostagemController {
 	
 	@PostMapping("/salvar")
 	public ResponseEntity<Postagem> salvar(@Valid @RequestBody Postagem novaPostagem){
+		return ResponseEntity.status(201).body(repository.save(novaPostagem));
+	}
+	
+	@PutMapping("/atualizar")
+	public ResponseEntity<Postagem> atualizar(@Valid @RequestBody Postagem novaPostagem) {
 		return ResponseEntity.status(201).body(repository.save(novaPostagem));
 	}
 	
