9c73ceb8217ca23cbb31e996e9a1e86202be6c50
GScabuzzi <gustavo-gsg@hotmail.com>
2021-09-28 01:09:32 -0300

    [Feature Scabuzzi - Task: ADD method POST]

--- src/main/java/org/generation/blogGScabuzzi/controller/PostagemController.java
+++ src/main/java/org/generation/blogGScabuzzi/controller/PostagemController.java
@@ -2,6 +2,8 @@ package org.generation.blogGScabuzzi.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.generation.blogGScabuzzi.model.Postagem;
 import org.generation.blogGScabuzzi.repository.PostagemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +11,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -41,5 +45,12 @@ public class PostagemController {
 	public ResponseEntity<List<Postagem>> GetByTexto(@PathVariable String texto){
 		return ResponseEntity.ok(repository.findAllByTextoContainingIgnoreCase(texto));
 	}
+	
+	@PostMapping("/salvar")
+	public ResponseEntity<Postagem> salvar(@Valid @RequestBody Postagem novaPostagem){
+		return ResponseEntity.status(201).body(repository.save(novaPostagem));
+	}
+	
+	
 	
 }
