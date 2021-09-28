d05aee6e6b317ad2ec6a23a2afe955a775c76a78
GScabuzzi <gustavo-gsg@hotmail.com>
2021-09-28 10:51:07 -0300

    [Feature Scabuzzi - ADD Documentacao no PostagemRepository]

--- src/main/java/org/generation/blogGScabuzzi/controller/PostagemController.java
+++ src/main/java/org/generation/blogGScabuzzi/controller/PostagemController.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 import org.generation.blogGScabuzzi.model.Postagem;
 import org.generation.blogGScabuzzi.repository.PostagemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -29,48 +30,61 @@ public class PostagemController {
 
 	@GetMapping
 	public ResponseEntity<List<Postagem>> GetAll() {
-		return ResponseEntity.ok(repository.findAll()); // findAllPostagem = um endPoint com a capacidade de trazer todas as postagens
+		return ResponseEntity.ok(repository.findAll()); // findAllPostagem = um endPoint com a capacidade de trazer
+														// todas as postagens
 	}
-	
+
 	@GetMapping("/id/{id}")
-	public ResponseEntity<Postagem> GetById(@PathVariable long id){
-		return repository.findById(id)
-				.map(resp -> ResponseEntity.ok(resp))
-				.orElse(ResponseEntity.notFound().build());
+	public ResponseEntity<Postagem> GetById(@PathVariable long id) {
+		return repository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
 	}
-	
+
 	@GetMapping("/titulo/{titulo}")
-	public ResponseEntity<List<Postagem>> GetByTitulo(@PathVariable String titulo){
+	public ResponseEntity<List<Postagem>> GetByTitulo(@PathVariable String titulo) {
 		return ResponseEntity.ok(repository.findAllByTituloContainingIgnoreCase(titulo));
 	}
-	
+
 	@GetMapping("/texto/{texto}")
-	public ResponseEntity<List<Postagem>> GetByTexto(@PathVariable String texto){
+	public ResponseEntity<List<Postagem>> GetByTexto(@PathVariable String texto) {
 		return ResponseEntity.ok(repository.findAllByTextoContainingIgnoreCase(texto));
 	}
-	
+
 	@PostMapping("/salvar")
-	public ResponseEntity<Postagem> salvar(@Valid @RequestBody Postagem novaPostagem){
+	public ResponseEntity<Postagem> salvar(@Valid @RequestBody Postagem novaPostagem) {
 		return ResponseEntity.status(201).body(repository.save(novaPostagem));
 	}
-	
-	@PutMapping("/atualizar")
+
+	@PutMapping("/atualizar") // METODO PUT ENSINADO EM AULA
 	public ResponseEntity<Postagem> atualizar(@Valid @RequestBody Postagem novaPostagem) {
 		return ResponseEntity.status(201).body(repository.save(novaPostagem));
 	}
-	
-	@DeleteMapping("/deletar/{id}")
-	public ResponseEntity<Postagem> deletar(@PathVariable(value = "id") Long idPostagem){
+
+	@PutMapping // METODO PUT ENSINADO EM VIDEO
+	public ResponseEntity<Postagem> put(@RequestBody Postagem postagem) {
+		return ResponseEntity.status(HttpStatus.OK).body(repository.save(postagem));
+	}
+
+	@DeleteMapping("/deletar/{id}") // METODO DELETE ENSINADO EM AULA
+	public ResponseEntity<Postagem> deletar(@PathVariable(value = "id") Long idPostagem) {
 		Optional<Postagem> objetoOptional = repository.findById(idPostagem);
-		
-		if(objetoOptional.isPresent()) {
+
+		if (objetoOptional.isPresent()) {
 			repository.deleteById(idPostagem);
 			return ResponseEntity.status(204).build();
-		}else {
+		} else {
 			return ResponseEntity.status(400).build();
 		}
 	}
-	
-	
-	
+
+	/**
+	 * Method delete -> Usado em nas video aulas
+	 * 
+	 * @author Gustavo Scabuzzi
+	 * @param id
+	 */
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable long id) {
+		repository.deleteById(id);
+	}
+
 }
--- src/main/java/org/generation/blogGScabuzzi/repository/PostagemRepository.java
+++ src/main/java/org/generation/blogGScabuzzi/repository/PostagemRepository.java
@@ -7,8 +7,25 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PostagemRepository extends JpaRepository<Postagem, Long>{
-	public List<Postagem> findAllByTituloContainingIgnoreCase (String titulo);
-	
-	public List<Postagem> findAllByTextoContainingIgnoreCase (String texto);
+public interface PostagemRepository extends JpaRepository<Postagem, Long> { // É Long pois a nosso chave primaria é Long
+
+	/**
+	 * Metodo utilizado para realizar pesquisa pela coluna titulo da tabela Postagem
+	 * 
+	 * @param titulo
+	 * @return lista com Postagem
+	 * @author Gustavo Scabuzzi
+	 * @since 1.0
+	 */
+	public List<Postagem> findAllByTituloContainingIgnoreCase(String titulo);
+
+	/**
+	 * Metodo utilizado para realizar pesquisa pela coluna texto da tabela Postagem
+	 * 
+	 * @param titulo
+	 * @return lista com Postagem
+	 * @author Gustavo Scabuzzi
+	 * @since 1.0
+	 */
+	public List<Postagem> findAllByTextoContainingIgnoreCase(String texto);
 }
