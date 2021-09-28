1b05a0bfafbcf4cbe4f7285fb8a1994174a6a528
GScabuzzi <gustavo-gsg@hotmail.com>
2021-09-28 13:33:59 -0300

    [Feature Scabuzzi - Task: ADD interface TemaRepository, ADD findAllByDescricaoContainingIgnoreCase]

---
+++ src/main/java/org/generation/blogGScabuzzi/repository/TemaRepository.java
@@ -0,0 +1,11 @@
+package org.generation.blogGScabuzzi.repository;
+
+import java.util.List;
+
+import org.generation.blogGScabuzzi.model.Tema;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TemaRepository extends JpaRepository<Tema, Long> {
+
+	public List<Tema> findAllByDescricaoContainingIgnoreCase(String descricao);
+}
